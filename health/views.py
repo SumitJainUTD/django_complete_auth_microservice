54a242584772453eb2cfef0d530061e5c081adac
SumitJainUTD <sumi12june@gmail.com>
2022-09-21 23:41:29 -0500

    health url added

--- health/views.py
+++ health/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET', ])
+def api_get_health(request):
+    return HttpResponse("OK")
