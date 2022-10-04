b9aa5955577f01a69528dd9fac5f9e9b708f699f
SumitJainUTD <sumi12june@gmail.com>
2022-10-03 22:44:14 -0500

    db updated added Readme.md

--- health/models.py
+++ health/models.py
@@ -1,3 +1,12 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+class Health(models.Model):
+    id = models.CharField(max_length=80, primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    token = models.UUIDField(auto_created=True, default=uuid.uuid4())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
